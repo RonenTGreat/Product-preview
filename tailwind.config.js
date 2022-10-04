71c1b1f7ad039599893ca8bf953cadab6e1d996c
ronentgreat <ronenh53@gmail.com>
2022-10-04 19:16:42 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        darkcyan: "hsl(158, 36%, 37%)",
+        cream: "hsl(30, 38%, 92%)",
+        verydarkblue: "hsl(212, 21%, 14%)",
+        grayishblue: "hsl(228, 12%, 48%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        heading: ["Fraunces"],
+        body: ["Montserrat"],
+      },
+    },
+  },
+  plugins: [],
+};
